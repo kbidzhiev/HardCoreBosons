2f8ba6360b7127a8dca441bb773bb5e385360785
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2020-12-19 15:08:40 +0100

    Eigen library first use

--- src/HardCoreBosons.cpp
+++ src/HardCoreBosons.cpp
@@ -1,15 +1,21 @@
 //============================================================================
 // Name        : HardCoreBosons.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : BK
+// Description : HardCoreBosons fredholm determinant
 //============================================================================
 
-#include <iostream>
-using namespace std;
+#include "../../cpp_libs/eigen/Eigen/Dense"
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+#include <iostream>
+
+using Eigen::MatrixXd;
+
+int main()
+{
+  MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  std::cout << m << std::endl;
 }
