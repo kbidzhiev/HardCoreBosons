029f6248e675416c99f0070dff6a1e09559c692c
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2020-12-19 21:01:22 +0100

    profiler to measure time, Fredholm kernels of XY model

--- src/HardCoreBosons.cpp
+++ src/HardCoreBosons.cpp
@@ -6,13 +6,71 @@
 
 #include "boost/math/quadrature/gauss.hpp"
 #include "../../cpp_libs/eigen/Eigen/Dense"
+#include "profile.h"
 
 
 #include <iostream>
-#include <math.h> // for definition of pi = 3.14...    M_PI
+//#include <math.h>	 // definition of M_PI = pi = 3.14...
+#include <cmath> 	 // pow (x,3) = x^3 = x*x*x
+#include <complex.h> // trigoniometric functions sin() cos()
 
 using Eigen::MatrixXd;
 using namespace boost::math::quadrature;
+
+using Cplx = complex<double>;
+
+const Cplx Cplx_i = Cplx(0,1);
+
+
+complex<double> Weight (const double momenta,
+		const double beta,
+		const double gamma,
+		const double magnetization){
+
+	const Cplx sqrt_term = sqrt(pow(magnetization-cos(momenta),2) + pow(gamma * sin(momenta),2));
+
+	complex<double> result =
+			magnetization - cos(momenta) - Cplx_i * gamma * sin(momenta) ;
+
+	result /= sqrt_term ;
+	result *= -tanh(beta * sqrt_term /2.);
+	result += 1;
+	result *= 0.5;
+
+
+	return result;
+}
+
+
+Cplx Kernel (const Cplx weight,
+		const double x,
+		const double momenta1,
+		const double momenta2
+		){
+	Cplx kernel = sin(0.5 * x * (momenta1-momenta2))/
+			sin(0.5 * x * (momenta1-momenta2));
+	Cplx result = -weight/M_PI;
+	if (abs (momenta1 - momenta2) > 1e-14 ){
+		result *= kernel;
+	} else {
+		result *= x;
+	}
+	return  result;
+}
+
+Cplx KernelFiniteRank (const Cplx kernel,
+		const Cplx weight,
+		const double x,
+		const double momenta1,
+		const double momenta2
+		){
+	Cplx result = kernel;
+	result += weight * exp(-Cplx_i * x * 0.5* (momenta1 + momenta2))
+		*exp(-Cplx_i * 0.5* (momenta1 - momenta2));
+	return result;
+}
+
+
 
 
 int main(){
@@ -29,9 +87,24 @@ int main(){
 	auto f = [](const double& t) { return std::exp(-t * t); };
 
 
-//	boost::multiprecision::cpp_bin_float_quad Q2 = gauss<boost::multiprecision::cpp_bin_float_quad, 20>::integrate(f2, -M_PI, M_PI);
+	double Q{0};
+	{
+		LOG_DURATION("gauss  30");
+		Q = gauss<double, 30>::integrate(f, -M_PI, M_PI);
+	}
+	{
+		LOG_DURATION("gauss  40");
+		Q = gauss<double, 40>::integrate(f, -M_PI, M_PI);
+	}
+	{
+		LOG_DURATION("gauss 100");
+		Q = gauss<double, 100>::integrate(f, -M_PI, M_PI);
+	}
+	{
+		LOG_DURATION("gauss 500");
+		Q = gauss<double, 500>::integrate(f, -M_PI, M_PI);
+	}
 
-	double Q = gauss<double, 10>::integrate(f, -M_PI, M_PI);
 
 
 	std::cout << std::setprecision(16)
