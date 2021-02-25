966ae7e569684368664b8b3b79d8a8371827c332
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2021-02-25 15:45:29 +0100

    subdir mk with faddeeva hh

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -3,12 +3,19 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+CC_SRCS += \
+../src/Faddeeva.cc 
+
 CPP_SRCS += \
 ../src/HardCoreBosons.cpp \
 ../src/PVintegration.cpp \
 ../src/kernel.cpp 
 
+CC_DEPS += \
+./src/Faddeeva.d 
+
 OBJS += \
+./src/Faddeeva.o \
 ./src/HardCoreBosons.o \
 ./src/PVintegration.o \
 ./src/kernel.o 
@@ -20,6 +27,13 @@ CPP_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cc
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++17 -O3 -Wall -c -fmessage-length=0 -fopenmp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
