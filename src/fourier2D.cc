5cde07746d1c91fd085770eb648b1b08a106fc8a
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2021-03-11 13:30:33 +0100

    t_50 x_50 N_1000

--- src/fourier2D.cc
+++ src/fourier2D.cc
@@ -16,12 +16,12 @@ using namespace std;
 
 void Fourier2D(){
 
-	size_t N1 = 50;
-	size_t N2 = 50;
+	size_t N1 = 1000;
+	size_t N2 = 1000;
 	size_t N = N1 * N2;
 	dcvector data(N);
 	dcvector data_fft(N);
-	dvector t1(N1);
+	dvector x1(N1);
 	dvector t2(N2);
 	dvector k1(N1);
 	dvector w2(N2);
@@ -31,11 +31,11 @@ void Fourier2D(){
 	//double w2 = 2 * M_PI * 3;
 	//double w3 = 2 * M_PI * 5;
 	//double w4 = 2 * M_PI * 7;
-	double xmax1 = 10.0;
-	double tmax2 = 10.0;
+	double xmax1 = 50.0;
+	double tmax2 = 50.0;
 	size_t counter = 0;
 	auto Box = [&](double x, double y){
-		if (abs(y) < 0.5 && abs(x) < 0.5){
+		if (abs(x) < 0.5 && abs(y) < 0.5){
 			return 1.0;
 		} else
 			return 0.0;
@@ -45,24 +45,24 @@ void Fourier2D(){
     	return exp(-(x*x + y*y)  );
     };
 
-
+//#pragma omp parallel for num_threads(omp_get_num_procs()) collapse(2)
 	for (size_t i = 0; i < N1; ++i) {
-		t1[i] = i * xmax1 / N1  - (xmax1 / 2.0);
 		for (size_t j = 0; j < N2; ++j) {
-			t2[j] = j * tmax2 / N2 - (tmax2 / 2.0);
-			//SpaceTime st(X_coordinate(t1[i]),T_time(t2[j] + 0.01));
+			x1[i] = i * xmax1 / N1  - (xmax1 / 2.0);
+			t2[j] = j * tmax2 / N2 + 0.01;
+			SpaceTime st(X_coordinate(x1[i]),T_time(t2[j]));
 			cout << "x= " << i+1 << " / " << N1
 			<< " ; t= " << j+1 << " / " << N2 << ";\t"
 			<< ++counter << " / " << N  << endl;
 
-			data[i * N2 + j] = Box(t1[i], t2[j]);//Grep(st);
-					//(sin(w1 * t1[i]) + sin(w2 * t1[i]))
+			data[i * N2 + j] = Grep(st);//Box(x1[i], t2[j] - 5.0);
+					//(sin(w1 * x1[i]) + sin(w2 * x1[i]))
 					//* (sin(w3 * t2[j]) + sin(w4 * t2[j]));
 		}
 	}
 
-    auto Filter = [](double x){
-    	if (abs(x)<1e-10) return 0.0;
+    auto LowValueFilter = [](double x){
+    	if (abs(x)<1e-14) return 0.0;
     	else return x;
     };
 
@@ -82,7 +82,7 @@ void Fourier2D(){
 	fh2 << "# k \tw \tRe[f(k, w)] \tIm[f(x,t)]\n";
 	for (size_t i = 0; i < N1; ++i) {
 		for (size_t j = 0; j < N2; ++j) {
-			fh1 << t1[i] << " \t";
+			fh1 << x1[i] << " \t";
 			fh1 << t2[j] << " \t";
 			fh1 << data[i * N2 + j].real() << " \t";
 			fh1 << data[i * N2 + j].imag() << "\n";
@@ -90,8 +90,8 @@ void Fourier2D(){
 			{
 				fh2 << k1[i] / (2 * M_PI) << " \t";
 				fh2 << w2[j] / (2 * M_PI) << " \t";
-				fh2 << Filter(data_fft[i * N2 + j].real()) * (4*xmax1 * tmax2) << " \t";
-				fh2 << Filter(data_fft[i * N2 + j].imag()) * (4*xmax1 * tmax2) << "\n";
+				fh2 << LowValueFilter(data_fft[i * N2 + j].real()) * (4*xmax1 * tmax2) << " \t";
+				fh2 << LowValueFilter(data_fft[i * N2 + j].imag()) * (4*xmax1 * tmax2) << "\n";
 
 			}
 		}
