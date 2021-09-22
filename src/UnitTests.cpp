6f1414cb2af6ef1a2d5bb3a0825ffa39023e45d3
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2021-09-22 15:16:42 +0200

    there is a problem in PV smwhere

--- src/UnitTests.cpp
+++ src/UnitTests.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void PV_comparison(){
 	Q_momenta q(10.5);
-	SpaceTime st(X_coordinate(1.7),T_time(10.6));
+	const SpaceTime st(X_coordinate(1.7),T_time(10.6));
 	Cplx old_value = PrincipalValue_old(q, st)/M_PI;
 	Cplx new_value = PrincipalValue(q, st)/M_PI;
 	Cplx erf_arg = (st.x - q.value * st.t) * (-1.0 + Cplx_i) / (2.0 * sqrt(st.t));
@@ -13,7 +13,6 @@ void PV_comparison(){
 			"PV_new = " << new_value << endl;
 	cout << "erf          = " << Faddeeva::erf(erf_arg) << '\n';
 	cout << "erf(wolfram) = (1.00787,-0.0223579)" << endl;
-
 }
 
 
