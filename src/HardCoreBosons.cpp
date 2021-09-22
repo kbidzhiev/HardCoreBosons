2e08b0e4003dd727281a75684abeab232cb6fce1
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2021-09-22 14:39:22 +0200

    comments on integration

--- src/HardCoreBosons.cpp
+++ src/HardCoreBosons.cpp
@@ -134,13 +134,18 @@ void PV(){
 }
 
 void Integrating(){
+	// Here I'm checking quality between Gauss and Gauss_Kronrod.
+	// Apparently Gauss requires large matrices to reach the same answer as Gauss_Kronrod has
+	// for small matrices
+	// M = 11 gives diff answers,
+	// whereas M = 61 is ok and agrees with Mathematica
 	using namespace boost::math::quadrature;
 	auto f=[](const double x){
 		return cos(x);
 	};
 	double error ;
-	double kron = gauss_kronrod<double, 61>::integrate(f, -50, 50,  10, 1e-9, &error);
-	double g = gauss<double, 61>::integrate(f, -50, 50);
+	double kron = gauss_kronrod<double, 11>::integrate(f, -50, 50,  10, 1e-9, &error);
+	double g = gauss<double, 11>::integrate(f, -50, 50);
 
 	//double Q = gauss<double, 7>::integrate(f, 0, 1);
 
